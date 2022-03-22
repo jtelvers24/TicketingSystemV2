1d821f958298814d55239e476cb48f5b3e232d33
John Elvers <jelvers@my.wctc.edu>
2022-03-21 22:03:21 -0500

    Added Write Enhancment Class

--- Program.cs
+++ Program.cs
@@ -34,7 +34,52 @@ namespace TicketingSystemV2
         }
         }
     }
-    
+    class WriteEnhancments
+    {
+        public void RunWriteFile()
+        {
+            string choice = " ";
+            StreamWriter sw = new StreamWriter("Enhancements.csv", append: true);
+                for (int i = 0; i < 7; i++)
+                {
+                    
+                    int ticketid = 0;
+                    ticketid +=1;
+                                            // ask a question
+                     Console.WriteLine("Enter a ticket (Y/N)?");
+                        // input the response
+                    string resp = Console.ReadLine().ToUpper();
+                        // if the response is anything other than "Y", stop asking
+                    if (resp != "Y") { break; }
+                        // prompt for course name
+                    Console.WriteLine("Enter Summary");
+                        // save the course name
+                    string summary = Console.ReadLine();
+                        // prompt for course grade
+                     Console.WriteLine("Status");
+                        // save the course grade
+                        string status = Console.ReadLine();
+                    Console.WriteLine("Priority");
+                        // save the course grade
+                    string priority = Console.ReadLine();
+                     Console.WriteLine("Submitter");
+                        // save the course grade
+                    string submitter = Console.ReadLine();
+                    Console.WriteLine("Assigned");
+                        // save the course grade
+                    string assigned = Console.ReadLine();
+                    Console.WriteLine("Watching");
+                        // save the course grade
+                    string watching = Console.ReadLine();
+                    sw.WriteLine("{0}|{1}|{2}|{3}|{4}|{5}|{6}", i, summary, status, priority, submitter, assigned, watching);
+                    
+                    sw.Close();
+                }while (choice == "1" || choice == "2");
+      
+
+        }
+    }
+
     class ReadEnhancements
     {
 public void RunReadFile(){
